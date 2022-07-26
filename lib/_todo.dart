bddc8e7262a14839db350d8ca1ec5982eb78523c
Harald Kriebisch <Harald.Kriebisch@gmx.de>
2022-07-26 15:40:52 +0200

    Add uuids to Todos
    The Todos where previously not unique, resulting in several being selected at once if the todoText was the same.
    Using uuids to make all todos unique fixed the issue

--- lib/_todo.dart
+++ lib/_todo.dart
@@ -1,6 +1,12 @@
+import 'package:uuid/uuid.dart';
+
+// Create uuid object
+var uuid = Uuid();
+
 class Todo {
   String todoText;
   String todoPriority;
+  String id;
 
-  Todo(this.todoText, this.todoPriority);
+  Todo(this.todoText, this.todoPriority, id) : id = id ?? uuid.v1();
 }
